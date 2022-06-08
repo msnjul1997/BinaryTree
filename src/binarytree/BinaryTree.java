a770c6b2d11b4cbf9ab95237706097d7541a4471
msnkittu859 <Vamshikalyan97>
2022-06-08 21:47:02 +0530

    Size Added

--- src/binarytree/BinaryTree.java
+++ src/binarytree/BinaryTree.java
@@ -25,6 +25,15 @@ private BinaryNode<K> root;
 		}
 		return current;
 	}
+	public int getSize() 
+	{
+		return this.getSizeBinary(root);
+	}
+	
+	public int getSizeBinary(BinaryNode<K> current)
+	{
+		return current == null ? 0 : 1 + this.getSizeBinary(current.left)+this.getSizeBinary(current.right);
+	}
 	@Override
 	public String toString() 
 	{
@@ -43,7 +52,19 @@ private BinaryNode<K> root;
 		binaryTree.add(56);
 		binaryTree.add(30);
 		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		int size = binaryTree.getSize();
 		System.out.println(binaryTree);
+		System.out.println("size of binary tree:"+size);
 	}
 
 }
